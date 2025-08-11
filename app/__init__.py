1e08cd546cfdd4f24cec79a6db35aef515554a03
Tanvirul Islam <tanvirhridoy1011@gmail.com>
2025-08-12 01:55:02 +0600

    basic set up in __init__py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create database object globally
+db=SQLAlchemy()
+def create_app():
+    app=Flask(__name__)
+    app.config['SECRECT_Key']='your-secrect-key'
+    app.config['SQLALCHEMY_DATABASE_URI']='sqllite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+    db.__init__(app)
+
+    from app.routes.auth import auth_bp
+    from app.routes.auth import task_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(task_bp)
+
+    return app
